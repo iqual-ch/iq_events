fe7b342fea09c283d15c53444901c6ed67811a16
ignovski <i.ginovski@iqual.ch>
2021-07-23 15:41:48 +0200

    Adjustments in views and forms

--- src/Plugin/WebformHandler/IqEventsWebformSubmissionHandler.php
+++ src/Plugin/WebformHandler/IqEventsWebformSubmissionHandler.php
@@ -26,18 +26,23 @@ class IqEventsWebformSubmissionHandler extends \Drupal\webform\Plugin\WebformHan
    */
   public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
 
-    \Drupal::logger('iq_events')->notice('Eyoo yo ');
     $values = $webform_submission->getData();
     $user = NULL;
     /*foreach ($values as $key => $element) {
 
     }*/
       if (!empty($values['iq_event_instance'])) {
-        $event_instance = Node::load($form_state->getValue($values['iq_event_instance']));
+        // @TODO: See how many people are registered with the webform.
+        $nr_persons = 1;
+        $event_instance = Node::load($values['iq_event_instance']);
         if (!empty($event_instance)) {
+          if (!empty($values['iq_nr_persons'])) {
+            $nr_persons += $values['iq_nr_persons'];
+          }
 
           $number_of_registrations = $event_instance->field_iq_number_registrations->value;
-          $event_instance->set('field_iq_number_registrations', $number_of_registrations - 1);
+
+          $event_instance->set('field_iq_number_registrations', $number_of_registrations + $nr_persons);
           $event_instance->save();
         }
       }
